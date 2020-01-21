07af3eb6702aa303e4000016f4b0ea8c39c4d8fa
Rafa <raphael.mota@upu.int>
2020-01-21 11:15:48 +0100

    First commit : Empty skeleton from DB -> DotnetCore web api -> vuejs

---
+++ dating-app.frontend/src/components/Home.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-container>
+    <v-layout text-center wrap>
+      <v-flex mt-4>
+        <h1 class="blue--text display-1 font-weight-bold my-12">
+          Welcome to Dating App
+        </h1>
+        <h1 class="display-2 font-weight-bold my-12">
+          Reading values from DB
+        </h1>
+        <ul id="listOfValues">
+          <li v-for="value in values" v-bind:key="value">
+            {{ value.id }} | {{ value.name }}
+          </li>
+        </ul>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      values: []
+    };
+  },
+  created() {
+    axios
+      .get(`http://localhost:5000/api/values`)
+      .then(response => {
+        // JSON responses are automatically parsed.
+        this.values = response.data;
+      })
+      .catch(e => {
+        this.errors.push(e);
+      });
+
+    // async / await version (created() becomes async created())
+    //
+    // try {
+    //   const response = await axios.get(`http://jsonplaceholder.typicode.com/posts`)
+    //   this.posts = response.data
+    // } catch (e) {
+    //   this.errors.push(e)
+    // }
+  }
+};
+</script>
