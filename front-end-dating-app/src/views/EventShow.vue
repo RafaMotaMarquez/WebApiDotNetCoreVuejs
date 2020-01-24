a14dd04a87dc471679f5ab6380792c62ef057079
Rafa <raphael.mota@upu.int>
2020-01-24 17:06:16 +0100

    Get single event method

--- front-end-dating-app/src/views/EventShow.vue
+++ front-end-dating-app/src/views/EventShow.vue
@@ -1,10 +1,29 @@
 <template>
-  <h1>Showing event # {{ id }}</h1>
+  <h1>Showing event {{ event.title }}</h1>
 </template>
 
 <script>
+import EventService from '@/_services/EventService.js'
+
 export default {
-  props: ['id']
+  props: ['id'],
+  data() {
+    return {
+      event: {}
+    }
+  },
+  created() {
+    EventService.getEvent(this.id)
+      .then(response => {
+        this.event = response.data
+        console.log('*/*/*/*/*/*')
+        console.log(this.event)
+      })
+      .catch(error => {
+        console.log('*/*/*/*/*/*')
+        console.log('Some error occured' + error.response)
+      })
+  }
 }
 </script>
 
